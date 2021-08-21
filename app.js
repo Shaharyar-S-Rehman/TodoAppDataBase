2243c6fead6110deb120af4df42904be05bbf047
Shaharyar-S-Rehman <64689533+Shaharyar-S-Rehman@users.noreply.github.com>
2021-08-21 19:40:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let signup = () => {
 
             firebase.database().ref(`users/${res.user.uid}`).set(user)
                 .then(() => {
-                    alert("User register hogaya")
+                    alert("Registration Successfully")
                     window.location.href = "login.html"
                 })
 
